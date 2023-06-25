652ec5df97db11bbf79b74c6cdc14f714952408b
+ <+>
2023-06-26 02:10:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+  // Test case for matchName
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, lowerName) {
+    return drivers.filter(driver => driver.toLowerCase() === lowerName.toLowerCase());
+  }
+console.log(findMatching())
+
+function fuzzyMatch(drivers,lowerName){
+    return drivers.filter(driver => driver[0] === lowerName[0])
+}
+
+function matchName(drivers,lowerName){
+
+    const matches= drivers.filter(driver => driver.name === lowerName);
+
+    return matches
+}
